49c399ced2fa08d99b4074ac91376607ad551395
Kavalenka Valeriy <Kavalenka.Valeriy@gmail.com>
2018-11-12 12:50:33 +0300

    trying to play a sound at work

--- script/script.js
+++ script/script.js
@@ -1,10 +1,11 @@
-$(document).ready(function(){
+$(window).on('load', function(){
 	var st = $('.rc'),
 		rb =$('.rb'),
 		dm = $('.dropIt'),
 		CLASS_VISIBLE = 'visible';
 	
 	$.fx.off=1;
+
 	st.click(function(){
 			dm.toggle(CLASS_VISIBLE);
 		});
@@ -14,10 +15,10 @@ $(document).ready(function(){
 	var Button_Click = 'click';
 
 	function loadSound(){
-	createjs.Sound.registerSound('../media/sounds/Button_Click.wav', Button_Click); 	
+	createjs.Sound.registerSound('../media/sounds/Button_Click%20%28online-audio-converter.com%29.mp3', Button_Click); 	
 	};
 
-	function playSound(){
+	function playSound(Button_Click){
 		createjs.Sound.play(Button_Click);
 	}
 
